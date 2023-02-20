5230aed18d71145be59f265c436585fd2abf80d4
David Goedicke <2676840+DavidGoedicke@users.noreply.github.com>
2023-02-20 14:18:48 -0500

    update scripts

---
+++ Lab4/test_camera.py
@@ -0,0 +1,33 @@
+# import the necessary packages
+import numpy as np
+import cv2
+ 
+# initialize the HOG descriptor/person detector
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+cv2.startWindowThread()
+
+# open webcam video stream
+cap = cv2.VideoCapture(0)
+
+# the output will be written to output.avi
+out = cv2.VideoWriter(
+    'output.avi',
+    cv2.VideoWriter_fourcc(*'MJPG'),
+    15.,
+    (640,480))
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+# and release the output
+frame.release()
+# finally, close the window
+cv2.destroyAllWindows()
+cv2.waitKey(1)
